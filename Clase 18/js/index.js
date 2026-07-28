7ea39bd8dcb80c3fd6745b52c45a1cb2426c858e
bouchra2024 <morenoo.leandra@alumnos.cei.es>
2026-07-28 10:49:03 +0200

    hotel

---
+++ Clase 18/js/index.js
@@ -0,0 +1,26 @@
+console.log('Hola')
+console.log(2*2)
+console.log(false)
+
+
+/* VARIABLES*/
+let edad = 30
+edad = 27
+console.log(edad) 
+
+/* Constante no cambia su valor, se crea y se queda asi */
+const nombre = 'Leandra'
+console.log('Mi nombre es ' + nombre + ' y tengo ' + edad + ' años.')
+console.log(`Me llamo ${nombre} y tengo ${edad} años.`)
+
+
+let animal = 'perros'
+const ciudad = 'Valencia'
+let numero = 2*3
+
+console.log (`Vivo en ${ciudad} y quiero ${numero} ${animal} en unos años.`)
+/*
+pedro.edad
+pedro.respira('rápido')
+
+ */
