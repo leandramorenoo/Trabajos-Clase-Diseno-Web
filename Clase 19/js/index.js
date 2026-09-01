435f88458cb9688cc03ba3a9e8f7c3cc22ca560a
bouchra2024 <morenoo.leandra@alumnos.cei.es>
2026-09-01 11:52:33 +0200

    js

---
+++ Clase 19/js/index.js
@@ -0,0 +1,73 @@
+/*Console solo tiene métodos */
+console.log ()
+/* 
+- El objeto es console
+- El . punto es como la llave para acceder a propiedades y métodos 
+- Log es un metodo de registro (registro)
+- () son argumentos 
+- Si uso un metodo necesito un argumento
+- Las variables son let y const (var pero ya no se utiliza en js)
+- Despues de una variable com let y const va un nombre y depsues el valor 
+-Const -> no cambia su valor, se crea y se queda asi
+- si es un metodo en parentesis ()
+- Si es una propiedad es con =
+- */
+
+let nombrePropio 
+nombrePropio = 'Pepe'
+const DNI = '123456'
+
+/* INCREMENTO ++ */
+let num = 0
+
+num++
+
+console.log(num)
+
+
+num++
+
+console.log(num)
+
+num++ 
+num++ 
+
+console.log (num)
+
+/*DECREMENTO */
+
+num--
+console.log (num)
+
+
+personalbar.nombre = 'Pepe' /* nombre es una propiedad*/
+let nombre = 'Juan'
+
+let numero = 0
+numero++
+numero+=5 
+
+console.log (numero)
+
+
+// OPERADORES DE COMPARACIÓN 
+4 == "4" // true
+4 === "4" // false
+4 != "4" // false
+4 !== "4" // true
+4 > 4  //false
+4 >= 4 // true
+4 < 4 //false
+3 <= 4 // true
+
+let edad = 15
+const mayorEdad = 18
+
+
+if (edad >= mayorEdad) {
+    console.log('Puedes entrar!')
+} else {
+    console.log ('No puedes entrar.')
+}
+    
+
---
+++ Clase 21/js/index.js
@@ -0,0 +1,64 @@
+/*
+console.log ('Holi')
+- Console solo tiene métodos
+- El objeto es console
+- El . punto es como la llave para acceder a propiedades y métodos 
+- Log es un metodo de registro (registro)
+- () son argumentos 
+- Si uso un metodo necesito un argumento () (aunque este vacio)
+- Las variables son let y const (var pero ya no se utiliza en js)
+- Let crea auna variable
+- Despues de una variable com let y const va un nombre y despues el valor 
+-Const -> no cambia su valor, se crea y se queda asi
+- si es un metodo en parentesis ()
+- Si es una propiedad es con =
+- */
+
+/* 
+let nombre = 'Pedro'
+nombre = 'Ale'
+
+- Let estoy declarando una variable y no la vuelvo a redeclarar
+- Let se puede cambiar el valor
+- nombre es el identificador 
+- = se abre
+- 'Pedro' es el valor 
+- Se suel escribir en cammelcase nombreAlumno
+*/
+
+let nombrePepa = 'Pepa'
+
+console.log (nombrePepa)
+
+/* 
+const NOMBRE = "Pepe"
+
+- Se usa para valores que -NO- cambian como un dni
+- Se suele escribir en mayusculas
+- 
+*/
+
+const DNI = ''
+
+/* 
+- string "" ''
+- boolean true/false
+- number 
+- null (no tiene valor)
+*/
+
+let nombre = 'Pepito'
+let templateString 
+let edad = 23
+
+console.log('Se llama ' + nombre + ' y tiene ' + edad + ' años')
+console.log (`Se llama ${nombre} y tiene ${edad} años opc 2`)
+
+let cantidad = 4
+let prenda = 'camisas'
+let color = 'rojas'
+
+
+
+console.log (`${nombre} tiene ${cantidad} ${prenda} ${color}`)
+console.log (`${nombrePepa} compro ${edad} dulces`)
